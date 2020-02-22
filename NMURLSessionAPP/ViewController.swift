278ccfe7ef74538cbd88da7ccf9be485654df419
navidmostafiz <navidmostafiz@gmail.com>
2020-02-22 09:54:30 -0500

    Initial Commit

---
+++ NMURLSessionAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NMURLSessionAPP
+//
+//  Created by Navid on 2/22/20.
+//  Copyright © 2020 UnitedStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
